3d7e2250dd6a7ece1f43aa6b521154d0fb1041b9
Bayramnazar Esenov <bayramnazaresenovv@gmail.com>
2024-05-25 20:49:58 +0300

    v5

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,5 +4,14 @@ public class Runner {
         //versiyon 1
         System.out.println("Hello GIT");
         System.out.println("git add. : staging araya alir.");
+
+        //versiyon 4
+        System.out.println("Hello!!");
+
+        //versiyon 5
+        System.out.println("Hello....");
+
+        //versiyon 6
+        System.out.println("");
     }
 }
